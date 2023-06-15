69fb2dfa843eb58b6de4301a590e46440c84f971
Вадим Тараш <r9utw@mail.ru>
2023-06-15 16:53:18 +0700

    rename task

---
+++ Task_2/Task_2.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите два числа");
+int a;
+int b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("max = ");
+
+if (a > b) {
+    Console.WriteLine(a);
+} else {
+    Console.WriteLine(b);
+}
