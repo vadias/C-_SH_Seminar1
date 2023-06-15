4e04739b6abcd0069d2e832890cb5edc62ce5e71
Вадим Тараш <r9utw@mail.ru>
2023-06-15 16:44:33 +0700

    add task8

---
+++ Task_8/Task_8.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int num = 2;
+while(num < n) {
+    Console.Write(num);
+    num = num + 2;
+    if(num < n) {Console.Write(",");}
+}
