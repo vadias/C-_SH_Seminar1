bc5a2a74080ac35459849c7b9d1644e7e914b354
Вадим Тараш <r9utw@mail.ru>
2023-06-15 15:26:56 +0700

    add task6

---
+++ Task_6/Task_6.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+res = a%2;
+if(res == 0) {
+    Console.WriteLine("число чётное");
+} else {
+    Console.WriteLine("число не чётное");
+}
